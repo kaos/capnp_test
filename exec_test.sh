99071032c1b7ed50ae8fa024cdbd5ee3ecd704fa
Andreas Stenius <git@astekk.se>
2013-09-24 11:29:53 +0200

    Nah, I've succumbed to bash not being able to do what I want without using the filesystem. Sigh.

--- exec_test.sh
+++ exec_test.sh
@@ -3,13 +3,9 @@
 # Usage: exec_test.sh <Action> <Test> [<TestDataType>]
 
 # If CAPNP_TEST_APP returns 127, it indicates that it didn't run that
-# test, hence we should skip it. But, as we run the whole sequence in
-# a pipeline, I've not found any way to break the pipe half-way
-# through, so it seems we're stuck with the trailing diff's and
-# decodes any way.. nargh (I don't want to hit the FS just to get rid
-# of the pipeline.. that seems just dumb).
+# test, hence we should skip it.
 
-set -o pipefail
+export STATUS=0
 
 function result_status
 {
@@ -28,52 +24,42 @@ function failed
 
 function skipped
 {
-    echo "SKIP TEST REQUESTED"
+    result_status ".. SKIP" $* >&2
 }
 
 function run_test
 {
-    ${CAPNP_TEST_APP} $1 $2
-    if [ $? -eq 127 ]; then
-        echo "SKIP TEST REQUESTED" >&2
-
-# PLEASE BASH, break my pipeline here!
-# oh well..
-        # shuffle some sensible data down the pipeline instead..
-        $3 $1 $2
-
-        return 127
-    fi
+    ${CAPNP_TEST_APP} $1 $2 ; STATUS=$?
 }
 
 function decode_result
 {
-    capnp decode --short test.capnp $1
+    [[ $STATUS == 0 ]] && capnp decode --short test.capnp $1 < test.bin
 }
 
 function check_result
 {
-    diff expect/$2.txt - \
-        && passed $* \
-        || failed $*
-}
+    [[ $STATUS == 0 ]] && { diff expect/$2.txt actual.txt ; STATUS=$? ;}
 
-function empty_struct
-{
-# Single segment with empty root struct, to avoid a nasty uncaught
-# exception message from capnp decode about premature EOF ;)
-    echo AAAAAAEAAAAAAAAAAAAAAA== | base64 --decode
+    case $STATUS in
+        0)   passed $* ;;
+        127) skipped $* ;;
+        *)   failed $*
+    esac
+
+    rm -f actual.txt
+    return $STATUS
 }
 
 printf "++ TEST [%s %s]\n" $1 $2 >&2
 
 case $1 in
     decode)
-        cat expect/$2.bin \
-            | run_test decode $2 skipped \
-            | check_result $1 $2 ;;
+        run_test decode $2 < expect/$2.bin > actual.txt
+        check_result $1 $2 ;;
     encode)
-        run_test encode $2 empty_struct \
-            | decode_result $3 \
-            | check_result $1 $2
+        run_test encode $2  > test.bin \
+            && decode_result $3 > actual.txt
+        rm -f test.bin
+        check_result $1 $2
 esac
